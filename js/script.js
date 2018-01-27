7d69428798a677b7f4c91975f2f2fed17e18d518
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-01-27 22:56:01 +0100

    Add more functions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, screen, system) {
   this.brand = brand;
   this.price = price;
   this.color = color;
+  this.screen = screen;
+  this.system = system;
 }
 
   Phone.prototype.printInfo = function() {
     $("#printinfo").empty();
 
-    $("#printinfo").html("<h1>The phone brand is " + this.brand + ", color is " + this.color + " and price is " + this.price + ".</h1>");
+    $("#printinfo").html("<h1>The phone brand is " + this.brand + ", color is " + this.color + ", with " + this.screen + " screen and " + this.system + " system for " + this.price + ".</h1>");
   };
 
-  var SamsungGalaxy = new Phone("Samsung", "1399 zł", "black");
-  var iPhone = new Phone("iPhone", "2040 zł", "white");
-  var OnePlus = new Phone ("OnePluse One", "1299 zł", "black");
+  var SamsungGalaxy = new Phone("Samsung", "1399 zł", "black", "Super AMOLED", "Android");
+  var iPhone = new Phone("iPhone", "2040 zł", "white", "IPS TFT", "Apple A9");
+  var OnePlus = new Phone ("OnePluse One", "1299 zł", "black", "IPS TFT", "Android");
 
   var samsungImg = "<img src='https://cdn4.tgdd.vn/Products/Images/42/71081/Slider/samsung-galaxy-s6-edge-4.jpg'>";
   var iphoneImg = "<img src='https://allegro.stati.pl/AllegroIMG/PRODUCENCI/APPLE/MKY12ZMA/b.jpg'>";
