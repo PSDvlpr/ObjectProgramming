58522a228c05781a1c2e6eb5223a7da0cbd4cb27
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-01-29 16:07:05 +0100

    Clean up some code

--- js/script.js
+++ js/script.js
@@ -7,33 +7,29 @@ function Phone(brand, price, color, screen, system) {
 }
 
   Phone.prototype.printInfo = function() {
-    $("#printinfo").empty();
-
     $("#printinfo").html("<h1>The phone brand is " + this.brand + ", color is " + this.color + ", with " + this.screen + " screen and " + this.system + " system for " + this.price + ".</h1>");
   };
 
-  var SamsungGalaxy = new Phone("Samsung", "1399 zł", "black", "Super AMOLED", "Android");
-  var iPhone = new Phone("iPhone", "2040 zł", "white", "IPS TFT", "Apple A9");
-  var OnePlus = new Phone ("OnePluse One", "1299 zł", "black", "IPS TFT", "Android");
+  var data = {
+    "SamsungGalaxy": {
+      el: new Phone("Samsung", "1399 zł", "black", "Super AMOLED", "Android"),
+      src: "<img src='https://cdn4.tgdd.vn/Products/Images/42/71081/Slider/samsung-galaxy-s6-edge-4.jpg'>"
+    },
+    "iPhone": {
+      el: new Phone("iPhone", "2040 zł", "white", "IPS TFT", "Apple A9"),
+      src: "<img src='https://allegro.stati.pl/AllegroIMG/PRODUCENCI/APPLE/MKY12ZMA/b.jpg'>"
+    },
+    "OnePlus": {
+      el: new Phone ("OnePluse One", "1299 zł", "black", "IPS TFT", "Android"),
+      src: "<img src='https://dyw7ncnq1en5l.cloudfront.net/optim/news/64/64047/1_oneplus-5-all.jpg'>"
+    }
+  };
 
-  var samsungImg = "<img src='https://cdn4.tgdd.vn/Products/Images/42/71081/Slider/samsung-galaxy-s6-edge-4.jpg'>";
-  var iphoneImg = "<img src='https://allegro.stati.pl/AllegroIMG/PRODUCENCI/APPLE/MKY12ZMA/b.jpg'>";
-  var plusImg = "<img src='https://dyw7ncnq1en5l.cloudfront.net/optim/news/64/64047/1_oneplus-5-all.jpg'>";
+  $('.phone').click(function(){
+    var id = $(this).attr('data-id');
+    $("#phoneImg").html($(data[id].src));
+    data[id].el.printInfo();
 
-  $("#samsung").on("click", function(){
-    $("#phoneImg").empty();
-    $(samsungImg).appendTo($("#phoneImg"));
-    SamsungGalaxy.printInfo();
-  });
-
-  $("#iphone").on("click", function(){
-    $("#phoneImg").empty();
-    $(iphoneImg).appendTo($("#phoneImg"));
-    iPhone.printInfo();
-  });
-
-  $("#oneplus").on("click", function(){
-    $("#phoneImg").empty();
-    $(plusImg).appendTo($("#phoneImg"));
-    OnePlus.printInfo();
+    $('#info div').removeClass('active');
+    $(this).addClass('active');
   });
